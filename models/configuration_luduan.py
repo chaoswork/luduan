1bcee8f81a3c8f1eaef34dd922a15268e55ea047
huangchao <huangchao.cpp@gmail.com>
2023-07-18 12:04:02 +0800

    runnable demo using Huggingface and nanoGPT

---
+++ models/configuration_luduan.py
@@ -0,0 +1,41 @@
+from transformers.configuration_utils import PretrainedConfig
+from transformers.utils import logging
+
+
+logger = logging.get_logger(__name__)
+
+
+class LuduanConfig(PretrainedConfig):
+    model_type = "luduan"
+    keys_to_ignore_at_inference = ["past_key_values"]
+
+    def __init__(
+            self,
+            vocab_size=64000,
+            n_embd=768,
+            n_layer=12,
+            n_head=12,
+            block_size=1024,
+            dropout=0.0,
+            bias=False,
+            pad_token_id=0,
+            bos_token_id=1,
+            eos_token_id=2,
+            tie_word_embeddings=False,
+            **kwargs,
+        ):
+        self.vocab_size = vocab_size
+        self.block_size = block_size
+        self.n_embd = n_embd
+        self.n_layer = n_layer
+        self.n_head = n_head
+        self.dropout = dropout
+        self.bias = bias
+#        self.use_cache = use_cache
+        super().__init__(
+            pad_token_id=pad_token_id,
+            bos_token_id=bos_token_id,
+            eos_token_id=eos_token_id,
+            tie_word_embeddings=tie_word_embeddings,
+            **kwargs,
+        )
---
+++ train.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Author:	Huang Chao (huangchao.cpp@gmail.com)
+Date: Mon Jul 17 19:37:29 2023
+Brief:
+"""
+
+
+from dataclasses import dataclass, field
+from typing import Dict, Optional, Sequence
+
+import transformers
+from transformers import AutoTokenizer
+from transformers import TextDataset, DataCollatorForLanguageModeling
+from transformers import Trainer
+
+from models.modeling_luduan import LuduanForCausalLM
+from models.configuration_luduan import LuduanConfig
+
+
+@dataclass
+class ModelArguments:
+    model_name_or_path: Optional[str] = field(default="facebook/opt-125m")
+    block_size: Optional[int] = field(default=1024)
+
+
+@dataclass
+class DataArguments:
+    data_path: str = field(default=None, metadata={"help": "Path to the training data."})
+
+
+
+def make_supervised_data_module(tokenizer: transformers.PreTrainedTokenizer, data_args, model_args) -> Dict:
+    """Make dataset and collator for supervised fine-tuning."""
+    train_dataset = TextDataset(tokenizer=tokenizer, file_path=data_args.data_path, block_size=model_args.block_size)
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+    return dict(train_dataset=train_dataset, eval_dataset=None, data_collator=data_collator)
+    
+
+def train():
+    parser = transformers.HfArgumentParser((ModelArguments, DataArguments, transformers.TrainingArguments))
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+    model = LuduanForCausalLM(LuduanConfig(block_size=model_args.block_size))
+    tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path, trust_remote_code=True)
+
+    data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args, model_args=model_args)
+    trainer = Trainer(model=model, tokenizer=tokenizer, args=training_args, **data_module)
+    trainer.train()
+    trainer.save_state()
+    trainer.save_model(output_dir=training_args.output_dir)
+
+
+if __name__ == "__main__":
+    train()    
+
+    
+
+    
